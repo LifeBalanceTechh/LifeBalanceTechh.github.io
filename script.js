30df6949b756ab587d8c935883b8e38ca92ab2be
LifeBalanceTechh <unomatiaspa@gmail.com>
2024-11-15 16:10:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,12 @@ function calcularCalorias() {
         resultadoElement.appendChild(mensajeError);
     }
 }
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
